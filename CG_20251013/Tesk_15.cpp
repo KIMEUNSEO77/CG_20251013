7e4cfc252824efceab498cf5a7e4b66999d2453c
eunseo <2023180012@tukorea.ac.kr>
2025-10-14 14:22:43 +0900

    무조건 배열 순서대로만 그려지게 완성.

--- CG_20251013/Tesk_15.cpp
+++ CG_20251013/Tesk_15.cpp
@@ -22,8 +22,10 @@ GLuint fragmentShader;
 
 GLuint axisVAO = 0, axisVBO = 0;   // 축들
 GLuint cubeVAO[6] = { 0, }, cubeVBO[6] = { 0 };   // 정육면체
+GLuint pyramidVAO[6] = { 0, }, pyramidVBO[6] = { 0 };   // 삼각뿔
 
 bool face[6] = { false, false, false, false, false, false }; // 면 켜기/끄기
+bool pFace[6] = {false, false, false, false, false, false }; // 삼각뿔 면
 
 // 축 데이터 (위치, 색상)
 GLfloat axisVertices[] = 
@@ -46,11 +48,26 @@ float cube[8][3] =
 	{0, 0.5f, 0}, {-0.5f, 0.5f, 0}, {-0.5f, 0.5f, 0.5f}, {0, 0.5f, 0.5f}
 };
 int faces[6][4] = {
-	{0, 1, 2, 3}, {4, 5, 6, 7}, {1, 2, 6, 5},
-	{4, 0, 3, 7}, {1, 0, 4, 5}, {2, 3, 7, 6}
+	{1, 0, 4, 5}, {0, 1, 2, 3},
+	{1, 2, 6, 5}, {4, 0, 3, 7},  
+	{4, 5, 6, 7}, {2, 3, 7, 6}
 };
 float faceColors[6][3] = {
 	{1,0,0}, {0,1,0}, {0,0,1}, {1,1,0}, {1,0,1}, {0,1,1}   // 색상
+};
+
+// 삼각뿔 vertex 좌표값
+float pyramid[5][3] =
+{
+	{0, 0, 0}, {-0.5f, 0, 0}, {-0.5f, 0, 0.5f}, {0, 0, 0.5f},
+	{-0.25f, 0.5f, 0.25f}
+};
+int pyramidFaces[6][3] = {
+	{0, 1, 3}, {1, 2, 3}, {1, 0, 4}, {0, 3, 4},   // 0, 1은 한 면
+	{1, 2, 4}, {2, 3, 4}
+};
+float pyramidColors[6][3] = {
+	{1,0,0}, {1,0,0}, {0,0,1}, {1,1,0}, {1,0,1}, {0,1,1}   // 색상
 };
 
 void InitCube()
@@ -84,6 +101,37 @@ void InitCube()
 		glGenBuffers(1, &cubeVBO[i]);
 		glBindVertexArray(cubeVAO[i]);
 		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO[i]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+		// 위치
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		// 색상
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+}
+void InitPyramid()
+{
+	for (int i = 0; i < 6; i++)   // 6면 각각
+	{
+		std::vector<GLfloat> vertices;
+		int v0 = pyramidFaces[i][0];
+		int v1 = pyramidFaces[i][1];
+		int v2 = pyramidFaces[i][2];
+		float* color = pyramidColors[i];
+		// 삼각형 1: v0, v1, v2
+		vertices.push_back(pyramid[v0][0]); vertices.push_back(pyramid[v0][1]); vertices.push_back(pyramid[v0][2]);
+		vertices.push_back(color[0]); vertices.push_back(color[1]); vertices.push_back(color[2]);
+		vertices.push_back(pyramid[v1][0]); vertices.push_back(pyramid[v1][1]); vertices.push_back(pyramid[v1][2]);
+		vertices.push_back(color[0]); vertices.push_back(color[1]); vertices.push_back(color[2]);
+		vertices.push_back(pyramid[v2][0]); vertices.push_back(pyramid[v2][1]); vertices.push_back(pyramid[v2][2]);
+		vertices.push_back(color[0]); vertices.push_back(color[1]); vertices.push_back(color[2]);
+		glGenVertexArrays(1, &pyramidVAO[i]);
+		glGenBuffers(1, &pyramidVBO[i]);
+		glBindVertexArray(pyramidVAO[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, pyramidVBO[i]);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 		// 위치
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -144,6 +192,31 @@ char* filetobuf(const char* file)
 	// Return the buffer 
 }
 
+// 육면체 랜덤 면 2개 키기
+void RandomFace()
+{
+	for (int i = 0; i < 6; i++) face[i] = false;
+	for (int i = 0; i < 2; )
+	{
+		int r = rand() % 6;
+		if (!face[r])
+		{
+			face[r] = true;
+			i++;
+		}
+	}	
+}
+
+// 사각뿔 랜덤 면 1개 & 밑면 키기
+void RandomPyramidFace()
+{
+	for (int i = 0; i < 6; i++) pFace[i] = false;
+	pFace[0] = true; // 밑면
+	pFace[1] = true; // 밑면
+	int r = rand() % 4 + 2;
+	pFace[r] = true;
+}
+
 GLvoid Keyboard(unsigned char key, int x, int y)
 {
 	switch (key)
@@ -172,6 +245,31 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		face[5] = true;
 		glutPostRedisplay();
 		break;
+
+	case '7':
+		pFace[2] = true;
+		glutPostRedisplay();
+		break;
+	case '8':
+		pFace[3] = true;
+		glutPostRedisplay();
+		break;
+	case '9':
+		pFace[4] = true;
+		glutPostRedisplay();
+		break;
+	case '0':
+		pFace[5] = true;
+		glutPostRedisplay();
+		break;
+	case 'c':
+		RandomFace();
+		glutPostRedisplay();
+		break;
+	case 't':
+		RandomPyramidFace();
+		glutPostRedisplay();
+		break;
 	case 'q':
 		exit(0);
 		break;
@@ -195,9 +293,11 @@ void main(int argc, char** argv)
 
 	InitAxis();   // 축 초기화
 	InitCube();   // 정육면체 초기화
+	InitPyramid();   // 삼각뿔 초기화
 
 	glEnable(GL_DEPTH_TEST); // 깊이 테스트 활성화
-	//glEnable(GL_CULL_FACE); // 면 제거
+	glEnable(GL_CULL_FACE); // 면 제거
+	glDepthFunc(GL_ALWAYS);   // 항상 통과
 
 	glutKeyboardFunc(Keyboard);
 	glutDisplayFunc(drawScene);
@@ -288,6 +388,15 @@ GLvoid drawScene()
 		if (!face[i]) continue; // 해당 면이 false이면 건너뜀
 		glBindVertexArray(cubeVAO[i]);
 		glDrawArrays(GL_TRIANGLES, 0, 6); // 각 면은 삼각형 2개(6정점)
+		glBindVertexArray(0);
+	}
+
+	// 삼각뿔 각 면 그리기
+	for (int i = 0; i < 6; ++i)
+	{
+		if (!pFace[i]) continue; // 해당 면이 false이면 건너뜀
+		glBindVertexArray(pyramidVAO[i]);
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 각 면은 삼각형 1개(3정점)
 		glBindVertexArray(0);
 	}
 
